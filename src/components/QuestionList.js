6b6cc42d2051272c71c139270f6f385f3ee2b2dd
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-01-18 19:40:32 +0300

    trial

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://localhost:4000/questions")
+    .then((res) => res.json())
+    .then((questions) => setQuestions(questions));
+  }, []);
+   console.log(questions);
+// render the return value of the map to the component
+const questionItem =questions.map((quiz)=>(
+  <QuestionItem 
+  key={quiz.id}
+  question={quiz}
+  />
+))
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItem}</ul>
     </section>
   );
 }
